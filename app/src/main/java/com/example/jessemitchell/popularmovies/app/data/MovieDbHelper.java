cd65ea7b6276336dfbcf6ed64d831a4cd042e752
Jesse Mitchell <jallmitch@>
2017-01-13 19:30:25 -0600

    Added DbHelper Class
    Modified MovieContract by adding/renaming a couple of columns for videos

--- app/src/main/java/com/example/jessemitchell/popularmovies/app/data/MovieDbHelper.java
+++ app/src/main/java/com/example/jessemitchell/popularmovies/app/data/MovieDbHelper.java
@@ -11,15 +11,38 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MovieDbHelper extends SQLiteOpenHelper
 {
 
-    public MovieDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
+    private static final int DATABASE_VERSION = 1;
+    static final String DATABASE_NAME = "movies.db";
+
+    public MovieDbHelper(Context context)
     {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db)
     {
+        // SQL for creating the two tables
+        final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + MovieContract.MovieEntry.TABLE_NAME +
+                " (" + MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY, " +
+                       MovieContract.MovieEntry.COLUMN_MOVIE_TITLE + " TEXT NOT NULL, " +
+                       MovieContract.MovieEntry.COLUMN_MOVIE__OVERVIEW + " TEXT NOT NULL, " +
+                       MovieContract.MovieEntry.COLUMN_MOVIE_RELEASE_DATE + " TEXT NOT NULL, " +
+                       MovieContract.MovieEntry.COLUMN_MOVIE_POSTER + " TEXT NOT NULL, " +
+                       MovieContract.MovieEntry.COLUMN_MOVIE_VOTE_AVERAGE + " REAL NOT NULL, " +
+                       MovieContract.MovieEntry.COLUMN_MOVIE_LIST_TYPE + " TEXT NOT NULL);";
 
+        final String SQL_CREATE_VIDEO_TABLE = "CREATE TABLE " + MovieContract.VideoEntry.TABLE_NAME +
+                " (" + MovieContract.VideoEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                       MovieContract.VideoEntry.COLUMN_MOVIE_KEY + " INTEGER NOT NULL, " +
+                       MovieContract.VideoEntry.COLUMN_VIDEO_ID + " TEXT NOT NULL, " +
+                       MovieContract.VideoEntry.COLUMN_VIDEO_NAME + " TEXT NOT NULL, " +
+                       MovieContract.VideoEntry.COLUMN_VIDEO_SITE + " TEXT NOT NULL, " +
+                       MovieContract.VideoEntry.COLUMN_VIDEO_KEY + " TEXT NOT NULL, " +
+                       MovieContract.VideoEntry.COLUMN_VIDEO_TYPE + " TEXT NOT NULL, " +
+                       MovieContract.VideoEntry.COLUMN_VIDEO_SIZE + " INTEGER NOT NULL, " +
+                 " FOREIGN KEY (" + MovieContract.VideoEntry.COLUMN_MOVIE_KEY + ") REFERENCES " +
+                MovieContract.MovieEntry.TABLE_NAME + " (" + MovieContract.MovieEntry._ID + ")";
     }
 
     @Override
